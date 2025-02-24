71fa11a911f8b7318824a1f72f263837b2345e4d
purijatinn@gmail.com <purijatinn@gmail.com>
2025-02-24 22:27:48 +0530

    Add WhatsApp link to the WhatsApp component for direct messaging

--- src/components/Whatsapp.jsx
+++ src/components/Whatsapp.jsx
@@ -3,7 +3,10 @@ import { TbBrandWhatsapp } from "react-icons/tb";
 const Whatsapp = () => {
 
   return <div className="bg-green-600 rounded-full p-2 fixed bottom-10 right-10 hover:scale-125 transition-all duration-400 ease-linear ">
-    <TbBrandWhatsapp className="text-4xl text-white " />
+    <a href="https://wa.me/917582822000" target="_blank" rel="noreferrer">
+
+    <TbBrandWhatsapp className="text-4xl text-white " /> 
+    </a>
   </div>;
 };
 
