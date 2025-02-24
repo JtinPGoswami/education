2c63a306ad790a7d868da549dcc0e5f5e1f7ff6f
purijatinn@gmail.com <purijatinn@gmail.com>
2025-02-24 09:39:43 +0530

    Remove unused video review files and improve conditional rendering for college rating and brochure download in CollegeCard and CollegeDetail components

--- src/components/CollegeCard.jsx
+++ src/components/CollegeCard.jsx
@@ -3,28 +3,36 @@ import { FaStar } from "react-icons/fa6";
 import { useCollege } from "../context/CollegeContext";
 import { useNavigate } from "react-router-dom";
 const CollegeCard = ({ college }) => {
-  const {selectCollege }=useCollege()
-  const navigate=useNavigate()
-  const handleButtonClick=()=>{
-selectCollege(college)
-navigate("/college-detail")
-  }
+  const { selectCollege } = useCollege();
+  const navigate = useNavigate();
+  const handleButtonClick = () => {
+    selectCollege(college);
+    navigate("/college-detail");
+  };
   return (
     <div className="space-y-2 box-border flex sm:flex-row flex-col justify-start gap-4  p-2 mx-2 rounded-lg border-2 sm:border-transparent shadow-2xl border-[#77d693]   hover:border-t-[#77d693] group transition-colors ease-linear duration-700 sm:h-36 sm:py-0 py-2">
-      <img src={college.logo} alt="" className="sm:w-[40%] sm:h-auto w-50 h-50 mx-auto" />
+      <img
+        src={college.logo}
+        alt=""
+        className="sm:w-[40%] sm:h-auto w-50 h-50 mx-auto"
+      />
       <div className="w-full">
         <h2 className="text-gray-900 flex flex-col justify-between h-16 pt-3 group-hover:text-[#77d693] text-base font-medium group-hover:scale-105 transition-transform ease-linear duration-200">
           {college.name}
         </h2>
         <p className="text-lg italic font-light">{college.location}</p>
-
         <div className="flex justify-between items-center">
-          <div className="flex gap-1 items-center ">
-            <FaStar className="text-yellow-400" />
-            <p className="text-base text-gray-600">{college.rating}</p>
-          </div>
+          {college.rating && (
+            <div className="flex gap-1 items-center ">
+              <FaStar className="text-yellow-400" />
+              <p className="text-base text-gray-600">{college.rating}</p>
+            </div>
+          )}
 
-          <button onClick={handleButtonClick} className="px-3 sm:group-hover:block sm:hidden transition-all duration-200 ease-linear cursor-pointer bg-green-500 text-white py-1 rounded-lg hover:bg-green-600  lg:text-base text-sm">
+          <button
+            onClick={handleButtonClick}
+            className="px-3 sm:group-hover:block sm:hidden transition-all duration-200 ease-linear cursor-pointer bg-green-500 text-white py-1 rounded-lg hover:bg-green-600  lg:text-base text-sm"
+          >
             View Detail
           </button>
         </div>
--- src/components/CollegeDetail.jsx
+++ src/components/CollegeDetail.jsx
@@ -3,11 +3,10 @@ import React, { useState } from "react";
 import { BsStarFill } from "react-icons/bs";
 import { FaLocationDot } from "react-icons/fa6";
 const CollegeDetail = ({ college }) => {
-   console.log(college.brochure);
-   
+  console.log(college.brochure);
+
   return (
     <section className="mt-8 border border-gray-300 rounded-xl overflow-hidden  w-full  ">
-      
       <div className="w-full h-[200px] overflow-hidden">
         <img
           className="w-full h-full object-cover"
@@ -16,24 +15,31 @@ const CollegeDetail = ({ college }) => {
         />
       </div>
 
-      
       <div className="px-3 py-3   pb-10">
-        <h1 className="sm:text-2xl text-xl font-bold text-green-500">{college.name}</h1>
+        <h1 className="sm:text-2xl text-xl font-bold text-green-500">
+          {college.name}
+        </h1>
         <div className="mt-2 flex sm:flex-row flex-col justify-between items-center gap-1 text-gray-700">
           <div className="flex gap-2 items-center font-medium">
             <FaLocationDot className="text-gray-700" />
             <p className="sm:text-base text-sm">{college.location}</p> |
-            <BsStarFill className="text-yellow-500" />
-            <p className="sm:text-base text-sm">{college.rating}</p>
+            {college.rating && (
+              <>
+                <BsStarFill className="text-yellow-500" />
+                <p className="sm:text-base text-sm">{college.rating}</p>
+              </>
+            )}
           </div>
           <div className="sm:flex grid grid-cols-2 gap-2 items-center flex-wrap">
-            {college.brochure&&<a
-              href={college.brochure}
-             download={college.brochure}
-              className={` bg-green-500 text-center text-white    px-2 py-1 rounded ${college.highlights.length > 1? "col-span-2" : ""}`}
-            >
-            Download Brochure
-            </a>}
+            {college.brochure && (
+              <a
+                href={college.brochure}
+                download={college.brochure}
+                className=" bg-green-500 text-center text-white    px-2 py-1 rounded  col-span-2"
+              >
+                Download Brochure
+              </a>
+            )}
             {college.highlights?.map((item, index) => (
               <span
                 key={index}
